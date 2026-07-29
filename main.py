de5fa8603be67a6741b9010ada488ff3b151909a
rvikram-07 <rvikram9486@gmail.com>
2026-07-29 21:18:07 +0530

    Changes done in main file

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 from login import LoginPage
 
-
 class TouristBookingSystem:
-    def _init_(self):
+    def __init__(self):
         LoginPage()
 if __name__ == "__main__":
     app = TouristBookingSystem()
