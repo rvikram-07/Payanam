99c18f17d22206a25ccb4d4c9eaf954cd6ea7136
rvikram-07 <rvikram9486@gmail.com>
2026-07-29 20:54:23 +0530

    main file created

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from login import LoginPage
+
+
+class TouristBookingSystem:
+    def _init_(self):
+        LoginPage()
+if _name_ == "_main_":
+    app = TouristBookingSystem()
